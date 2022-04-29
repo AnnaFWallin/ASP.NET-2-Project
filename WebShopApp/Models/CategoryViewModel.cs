d8660c6e4601ab60fd5a68fee1737b48be7d2957
leventym86@hotmail.com <leventym86@hotmail.com>
2022-04-29 16:45:06 +0200

    created partial views and edited home page/index view

---
+++ WebShopApp/Models/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebShopApp.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
