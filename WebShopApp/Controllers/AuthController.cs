bef770a6ff34de135975ceba623c2acd92380695
anna <Anna.Wallin@yh.nackademin.se>
2022-05-06 14:10:01 +0200

    #46-Identity added new branch

---
+++ WebShopApp/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShopApp.Data;
+using WebShopApp.Models;
+
+namespace WebShopApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _db;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ApplicationDbContext db)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _db = db;
+        }
+        //public IActionResult Index(string returnUrl = null!)
+        public IActionResult Index()
+        {
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                //My Account - ordrar och wishlist kanske?
+                return View();
+            }
+            else
+            {
+                //Registrering och login - forms
+                return RedirectToAction("Login");
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register(string returnUrl = null!)
+        {
+            if(_signInManager.IsSignedIn(User))
+                return RedirectToAction("Index");
+
+            var form = new RegisterFormModel();
+
+            if(returnUrl != null)
+                form.ReturnUrl = returnUrl;
+
+            return View(form);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterFormModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityUser = new IdentityUser() { UserName = form.UserName, Email = form.Email };
+                
+                var result = await _userManager.CreateAsync(identityUser, form.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(identityUser, isPersistent: false);
+
+                    if(form.ReturnUrl == null || form.ReturnUrl == "/")
+                        return RedirectToAction("Index", "Home");
+                    else
+                        return LocalRedirect(form.ReturnUrl);
+                }
+
+            }
+
+
+            ViewData["ErrorMessage"] = "The registration failed! Try again";
+            return View();
+        }
+
+    }
+}
---
+++ WebShopApp/Models/RegisterFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopApp.Models
+{
+    public class RegisterFormModel
+    {
+        [Required (ErrorMessage = "You need to put in a Username")]
+        public string UserName { get; set; } = null!;
+
+        [Required (ErrorMessage = "You need to put in an Email address")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required (ErrorMessage = "You need to put in a Password")]
+        [DataType (DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
