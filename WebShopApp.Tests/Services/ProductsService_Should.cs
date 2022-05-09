050a0cf5988154bfd4932f2b6b2bf09c857a1caf
oliaxe <olivia.axellie@gmail.com>
2022-05-09 12:18:17 +0200

    name change

--- WebShopApp.Tests/Services/ProductsService_Should.cs
+++ WebShopApp.Tests/Services/ProductsService_Should.cs
@@ -12,7 +12,7 @@ namespace WebShopApp.Tests.Services
     public class ProductsService_Should
     {
         [Fact]
-        public async Task ProductService_Should_Return_List_Of_Products()
+        public async Task ProductService_Should_Return_Type_Products()
         {
             // Arrange
             var sut = new Mock<IProductService>();
@@ -25,7 +25,7 @@ namespace WebShopApp.Tests.Services
         }
 
         [Fact]
-        public async Task GetAllProductsAsync_Returns_ListOfProducts()
+        public async Task GetAllProducts_Returns_ListOfProducts()
         {
             // Arrange
             var sut = new Mock<IProductService>();
