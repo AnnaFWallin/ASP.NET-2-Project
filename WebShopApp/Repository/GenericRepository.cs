dd6eff7ec05ee1fe0fbb8fd8d184e46e7cac9681
anna <Anna.Wallin@yh.nackademin.se>
2022-05-17 17:32:01 +0200

    Added GenericRepository

---
+++ WebShopApp/Repository/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShopApp.Data;
+
+namespace WebShopApp.Repository
+{
+    public abstract class GenericRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        protected GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected virtual async Task<TEntity> ReadAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id) ?? null!;
+        }
+
+        protected virtual async Task<List<TEntity>> ReadAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync() ?? null!;
+        }
+    }
+    
+}
