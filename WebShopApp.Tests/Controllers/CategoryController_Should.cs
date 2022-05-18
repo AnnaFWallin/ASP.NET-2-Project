9c9d15576a7b02685059ad553239421c56d29d3c
Levent <leventym86@hotmail.com>
2022-05-18 19:28:10 +0200

    added categorycontroller test

---
+++ WebShopApp.Tests/Controllers/CategoryController_Should.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebShopApp.Controllers;
+using WebShopApp.Interfaces;
+using WebShopApp.Services;
+using WebShopApp.Tests.Fixtures;
+using Xunit;
+
+namespace WebShopApp.Tests
+{
+    public class CategoryController_Should
+    {
+
+        [Fact]
+        public async Task CategoryController_Should_Return_ViewCategoryResult()
+        {
+            // Arrange 
+            var categoryServiceMock = new Mock<ICategoryService>();
+            categoryServiceMock.Setup(x => x.GetAsync(CancellationToken.None))
+                .ReturnsAsync(CategoryFixtures.GetTestCategoriesEntity());
+
+            var sut = new CategoryController(categoryServiceMock.Object);
+
+            // Act
+            var result = await sut.Index(CancellationToken.None);
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+    }
+}
---
+++ WebShopApp.Tests/Fixtures/CategoryFixtures.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using WebShopApp.Models;
+using WebShopApp.Models.Entities;
+
+namespace WebShopApp.Tests.Fixtures
+{
+    public static class CategoryFixtures
+    {
+        public static IEnumerable<CategoryModel> GetTestCategories() => new List<CategoryModel>
+        {
+            new CategoryModel { Id = 1, Name = "Men's", ProductCount = 1, ImagePreview = "images/mens-clothing.jpg" },
+            new CategoryModel { Id = 2, Name = "Women's", ProductCount = 2, ImagePreview = "images/womens-clothing.jpg" },
+            new CategoryModel { Id = 3, Name = "Kids", ProductCount = 3, ImagePreview = "images/kids-clothing.jpg" },
+            new CategoryModel { Id = 4, Name = "Hats", ProductCount = 2, ImagePreview = "images/hats.jpg" },
+            new CategoryModel { Id = 5, Name = "Sunglasses", ProductCount = 4, ImagePreview = "images/sunglasses.jpg" },
+            new CategoryModel { Id = 6, Name = "Shoes", ProductCount = 5, ImagePreview = "images/shoes.jpg" },
+            new CategoryModel { Id = 7, Name = "Watches", ProductCount = 2, ImagePreview = "images/watches.jpg" }
+
+        };
+
+        public static IEnumerable<CategoryEntity> GetTestCategoriesEntity() => new List<CategoryEntity>
+        {
+            new CategoryEntity { Id = 1, Name = "Men's", ImagePreview = "images/mens-clothing.jpg" },
+            new CategoryEntity { Id = 2, Name = "Women's", ImagePreview = "images/womens-clothing.jpg" },
+            new CategoryEntity { Id = 3, Name = "Kids", ImagePreview = "images/kids-clothing.jpg" },
+            new CategoryEntity { Id = 4, Name = "Hats", ImagePreview = "images/hats.jpg" },
+            new CategoryEntity { Id = 5, Name = "Sunglasses", ImagePreview = "images/sunglasses.jpg" },
+            new CategoryEntity { Id = 6, Name = "Shoes", ImagePreview = "images/shoes.jpg" },
+            new CategoryEntity { Id = 7, Name = "Watches", ImagePreview = "images/watches.jpg" }
+
+        };
+    }
+}
