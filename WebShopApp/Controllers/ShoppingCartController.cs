a7b20fc7d9b3a7e651ac9f5f6329fb7f42917c22
anna <Anna.Wallin@yh.nackademin.se>
2022-05-18 08:51:31 +0200

    Added RemoveFromCart in ShoppingCartController

--- WebShopApp/Controllers/ShoppingCartController.cs
+++ WebShopApp/Controllers/ShoppingCartController.cs
@@ -42,5 +42,28 @@ namespace WebShopApp.Controllers
             HttpContext.Session.SetString("ShoppingCart", JsonConvert.SerializeObject(shoppingCart));
             return RedirectToAction("Index", "Home");
         }
+        public async Task<IActionResult> RemoveFromCart(int id)
+        {
+            var shoppingCart = new ShoppingCartModel();
+            var cartSession = HttpContext.Session.GetString("ShoppingCart");
+
+            if (!string.IsNullOrEmpty(cartSession))
+            {
+                shoppingCart = JsonConvert.DeserializeObject<ShoppingCartModel>(cartSession);
+
+                var index = shoppingCart.ShoppingCart.FindIndex(x => x.Product.Id == id);
+                if (index > -1)
+                    shoppingCart.ShoppingCart[index].Quantity -= 1;
+                else
+                    shoppingCart.ShoppingCart.Remove(shoppingCart.ShoppingCart[index]);
+            }
+            else
+            {
+                shoppingCart.ShoppingCart.Remove(shoppingCart.ShoppingCart[id]);
+            }
+
+            HttpContext.Session.SetString("ShoppingCart", JsonConvert.SerializeObject(shoppingCart));
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
