79a2e0556d51a5676618cec24489b228b479179d
anna <Anna.Wallin@yh.nackademin.se>
2022-05-18 07:47:17 +0200

    Added ShoppingCartController

---
+++ WebShopApp/Controllers/ShoppingCartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopApp.Repository;
+
+namespace WebShopApp.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ShoppingCartController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
