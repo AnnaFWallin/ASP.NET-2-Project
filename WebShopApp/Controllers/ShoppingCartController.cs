69c0f4111d6dfa8ef197d890132bfef66bc0582b
anna <Anna.Wallin@yh.nackademin.se>
2022-05-18 08:43:42 +0200

    Added method AddToCart in ShoppingCartController

--- WebShopApp/Controllers/ShoppingCartController.cs
+++ WebShopApp/Controllers/ShoppingCartController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebShopApp.Models;
 using WebShopApp.Repository;
 
 namespace WebShopApp.Controllers
@@ -17,9 +19,27 @@ namespace WebShopApp.Controllers
             return View();
         }
 
-        public IActionResult AddToCart(int id)
+        public async Task<IActionResult> AddToCart(int id)
         {
+            var shoppingCart = new ShoppingCartModel();
+            var cartSession = HttpContext.Session.GetString("ShoppingCart");
 
+            if (!string.IsNullOrEmpty(cartSession))
+            {
+                shoppingCart = JsonConvert.DeserializeObject<ShoppingCartModel>(cartSession);
+
+                var index = shoppingCart.ShoppingCart.FindIndex(x => x.Product.Id == id);
+                if (index > -1)
+                    shoppingCart.ShoppingCart[index].Quantity += 1;
+                else
+                    shoppingCart.ShoppingCart.Add(new CartItemModel { Product = await _productRepository.GetAsync(id) });
+            }
+            else
+            {
+                shoppingCart.ShoppingCart.Add(new CartItemModel { Product = await _productRepository.GetAsync(id) });
+            }
+
+            HttpContext.Session.SetString("ShoppingCart", JsonConvert.SerializeObject(shoppingCart));
             return RedirectToAction("Index", "Home");
         }
     }
